8d4c618c8bf82f0c21f233bed09cbf23ed79612e
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2020-11-25 23:04:11 +0100

    Refactored Home Page with Material UI Card and Grid List

--- frontend/youtube-clone-ui/src/actions/index.js
+++ frontend/youtube-clone-ui/src/actions/index.js
@@ -28,7 +28,7 @@ export const initVideo = file => {
     return axios.post('http://localhost:8080/videos', {}, {
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImpiMnZhOTNvMmZ4cWxqd1ZmdVctWiJ9.eyJpc3MiOiJodHRwczovL3F1b3RlbWFjaGluZS5ldS5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMTc1MzAzMTY2NjI0OTIxODcyOTciLCJhdWQiOlsiaHR0cDovL2xvY2FsaG9zdDo4MDgwIiwiaHR0cHM6Ly9xdW90ZW1hY2hpbmUuZXUuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTYwNTgyMDkyMCwiZXhwIjoxNjA1ODI4MTIwLCJhenAiOiJtQ3M2SmlWSnpPYjIyMnlUSUhFYndjZGlmV0dCYVpDRSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwifQ.gpJcyRwb2VZF2ynpjVHIQyqgZDvwokGsjFjONXC6mo5f649l-Jq6N8gQLNuj8zehDYNm6KkCC8PCrd1bRmQqBcjeE1HgyVQ58tOhYuZk5VKc1J_-shjDkun0ADlV60J1No44rPs-5Axw5-IITwfBm3Ir0lxfxAHhviDkweEboLndB1SD89xtbBeYI6kaX571NRdECR6iYHxMJNIOPd2CKgWLDklDe7J9bN4ryHh5wcQgQ9XUVSu7PkKyGVkv3wZnAn7PmW4b862TuKkFHTU0i_EmRU3iAoPJBV4zGsAMOFracWSmFuIIqyobKALNUuWO5uS2UM-bwuk-FYFegIySKQ"
+            "Authorization": "Bearer "
         }
     })
         .then(res => res.data)
@@ -41,7 +41,7 @@ export const uploadVideo = (file, url) => {
     return axios.post(url, formData, {
         headers: {
             "Content-Type": "multipart/form-data",
-            "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImpiMnZhOTNvMmZ4cWxqd1ZmdVctWiJ9.eyJpc3MiOiJodHRwczovL3F1b3RlbWFjaGluZS5ldS5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMTc1MzAzMTY2NjI0OTIxODcyOTciLCJhdWQiOlsiaHR0cDovL2xvY2FsaG9zdDo4MDgwIiwiaHR0cHM6Ly9xdW90ZW1hY2hpbmUuZXUuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTYwNTgyMDkyMCwiZXhwIjoxNjA1ODI4MTIwLCJhenAiOiJtQ3M2SmlWSnpPYjIyMnlUSUhFYndjZGlmV0dCYVpDRSIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwifQ.gpJcyRwb2VZF2ynpjVHIQyqgZDvwokGsjFjONXC6mo5f649l-Jq6N8gQLNuj8zehDYNm6KkCC8PCrd1bRmQqBcjeE1HgyVQ58tOhYuZk5VKc1J_-shjDkun0ADlV60J1No44rPs-5Axw5-IITwfBm3Ir0lxfxAHhviDkweEboLndB1SD89xtbBeYI6kaX571NRdECR6iYHxMJNIOPd2CKgWLDklDe7J9bN4ryHh5wcQgQ9XUVSu7PkKyGVkv3wZnAn7PmW4b862TuKkFHTU0i_EmRU3iAoPJBV4zGsAMOFracWSmFuIIqyobKALNUuWO5uS2UM-bwuk-FYFegIySKQ"
+            "Authorization": "Bearer "
         }
     })
         .then(res => res.data)
